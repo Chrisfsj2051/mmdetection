3b5692c01306de1b3436b0fba1e3cdf8413677f0
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2020-05-28 21:11:32 +0800

    Revert "add res2net backbone (#2237)"
    This reverts commit 6730084972938f374dcb535c34765b47c54a8c06.

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -1,7 +1,6 @@
 from .hrnet import HRNet
-from .res2net import Res2Net
 from .resnet import ResNet, ResNetV1d
 from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 
-__all__ = ['ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net']
+__all__ = ['ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet']
