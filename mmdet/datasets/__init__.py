06fab462a4a7f771942c77c872dd04f4f2b0d955
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2020-05-28 21:11:34 +0800

    Revert "add ClassBalancedDataset (#2721)"
    This reverts commit f745a64d62ff4b7619e1387453168eaea09e19b9.

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -2,8 +2,7 @@ from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
 from .cityscapes import CityscapesDataset
 from .coco import CocoDataset
 from .custom import CustomDataset
-from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
-                               RepeatDataset)
+from .dataset_wrappers import ConcatDataset, RepeatDataset
 from .samplers import DistributedGroupSampler, DistributedSampler, GroupSampler
 from .voc import VOCDataset
 from .wider_face import WIDERFaceDataset
@@ -13,6 +12,5 @@ __all__ = [
     'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset',
     'CityscapesDataset', 'GroupSampler', 'DistributedGroupSampler',
     'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
-    'ClassBalancedDataset', 'WIDERFaceDataset', 'DATASETS', 'PIPELINES',
-    'build_dataset'
+    'WIDERFaceDataset', 'DATASETS', 'PIPELINES', 'build_dataset'
 ]
