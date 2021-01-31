f051b91bd4214317b9db02ee91019ab16d865da3
Chrisfsj2051 <34179691+Chrisfsj2051@users.noreply.github.com>
2021-01-31 20:41:32 +0800

    upd

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 # we tested under slurm environment with Tesla V100 GPU.
 # please replace 'pat_mars1' with the available partition name of your environment
-bash tools/slurm_test.sh pat_mars1 train_task configs/tianchi/my_config.py mmdet_output/results
+# please make sure that your checkpoint path is ../user_data/model_data/ckpt.pth
+bash tools/slurm_test.sh pat_mars1 train_task configs/tianchi/my_config.py ../user_data/model_data/ckpt.pth --format-only
+mv submit.json ../prediction_result/result.json
